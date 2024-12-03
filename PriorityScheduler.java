a77ed5bf224309a9f0f3489a8cf11340a0c53059
Omar-Mahmoud-25 <omar.mahmoud25102004@gmail.com>
2024-12-03 09:43:14 +0200

    edited the structure

---
+++ PriorityScheduler.java
@@ -0,0 +1,12 @@
+public class PriorityScheduler extends CPUSchedular {
+    
+    public PriorityScheduler(Process[] processes) {
+        super(processes);
+    }
+    
+    @Override
+    public void execute() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
+
---
+++ ShortestJobFirstSchedular.java
@@ -0,0 +1,12 @@
+public class ShortestJobFirstSchedular extends CPUSchedular{
+
+    public ShortestJobFirstSchedular(Process[] processes) {
+        super(processes);
+    }
+
+    @Override
+    public void execute() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
