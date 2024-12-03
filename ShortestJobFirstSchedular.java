c46c9c10207c260dcc998b60d6746611ee5053e9
Omar-Mahmoud-25 <omar.mahmoud25102004@gmail.com>
2024-12-03 10:07:50 +0200

    added SJF

--- ShortestJobFirstSchedular.java
+++ ShortestJobFirstSchedular.java
@@ -1,12 +1,19 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class ShortestJobFirstSchedular extends CPUSchedular{
 
     public ShortestJobFirstSchedular(Process[] processes) {
         super(processes);
+        Arrays.sort(this.processes,Comparator.comparing(p -> p.getBurstTime()));
     }
 
     @Override
     public void execute() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        for (Process process : processes){
+            process.process(process.getRemainingTime());
+            System.out.println("processed: " + process);
+        }
     }
     
 }
